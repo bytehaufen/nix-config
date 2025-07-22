5cac8f161adb82ba631dffb554d6639349b4fbd1
bytehaufen <rico.ukro@gmx.de>
2025-07-22 11:15:06 +0200

    feat: make tui apps optional

--- modules/home/tui/neovim/vrapper/default.nix
+++ modules/home/tui/neovim/vrapper/default.nix
@@ -11,40 +11,42 @@
     zsh -c "source /home/rico/.config/zsh/.zshrc && kitty zsh -c \"cd /home/rico/.config/vrapper && python3 gen-cheatsheet.py && ${lib.getExe pkgs.glow} -p /tmp/cheatsheet.md\""
   '';
 in {
-  # For eclipses vim plugin
-  home.file.".vrapperrc".text =
-    # vim
-    ''
-      source ${vrapperPath}/files.vim
-      source ${vrapperPath}/buffers.vim
-      source ${vrapperPath}/code.vim
-      source ${vrapperPath}/debug.vim
-      source ${vrapperPath}/editor.vim
-      source ${vrapperPath}/git.vim
-      source ${vrapperPath}/goto.vim
-      source ${vrapperPath}/help.vim
-      source ${vrapperPath}/insertion.vim
-      source ${vrapperPath}/java.vim
-      source ${vrapperPath}/quit.vim
-      source ${vrapperPath}/refactoring.vim
-      source ${vrapperPath}/search.vim
-      source ${vrapperPath}/settings.vim
-      source ${vrapperPath}/sneak.vim
-      source ${vrapperPath}/tests.vim
-      source ${vrapperPath}/ui.vim
-      source ${vrapperPath}/windows.vim
-      source ${vrapperPath}/zoom.vim
-    '';
+  config = lib.mkIf config.opts.home.tui.enable {
+    # For eclipses vim plugin
+    home.file.".vrapperrc".text =
+      # vim
+      ''
+        source ${vrapperPath}/files.vim
+        source ${vrapperPath}/buffers.vim
+        source ${vrapperPath}/code.vim
+        source ${vrapperPath}/debug.vim
+        source ${vrapperPath}/editor.vim
+        source ${vrapperPath}/git.vim
+        source ${vrapperPath}/goto.vim
+        source ${vrapperPath}/help.vim
+        source ${vrapperPath}/insertion.vim
+        source ${vrapperPath}/java.vim
+        source ${vrapperPath}/quit.vim
+        source ${vrapperPath}/refactoring.vim
+        source ${vrapperPath}/search.vim
+        source ${vrapperPath}/settings.vim
+        source ${vrapperPath}/sneak.vim
+        source ${vrapperPath}/tests.vim
+        source ${vrapperPath}/ui.vim
+        source ${vrapperPath}/windows.vim
+        source ${vrapperPath}/zoom.vim
+      '';
 
-  xdg.configFile.vrapper.source = config.lib.file.mkOutOfStoreSymlink "${configPath}/vrapper";
+    xdg.configFile.vrapper.source = config.lib.file.mkOutOfStoreSymlink "${configPath}/vrapper";
 
-  xdg.desktopEntries = {
-    Vrapper-Cheatsheet = {
-      name = "Vrapper Cheatsheet";
-      exec = "${cheatsheet}";
-      terminal = false;
-      type = "Application";
-      categories = ["Development"];
+    xdg.desktopEntries = {
+      Vrapper-Cheatsheet = {
+        name = "Vrapper Cheatsheet";
+        exec = "${cheatsheet}";
+        terminal = false;
+        type = "Application";
+        categories = ["Development"];
+      };
     };
   };
 }
--- modules/home/tui/ssh.nix
+++ modules/home/tui/ssh.nix
@@ -1,31 +1,37 @@
-{config, ...}: {
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
+{
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.opts.home.tui.enable {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
 
-    matchBlocks = {
-      "*" = {
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
-      };
+      matchBlocks = {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
 
-      "github.com" = {
-        hostname = "ssh.github.com";
-        user = "git";
-        identityFile = config.age.secrets.id_ed25519_github.path;
-        identitiesOnly = true; # FIXME: check
-        addKeysToAgent = "yes";
+        "github.com" = {
+          hostname = "ssh.github.com";
+          user = "git";
+          identityFile = config.age.secrets.id_ed25519_github.path;
+          identitiesOnly = true;
+          addKeysToAgent = "yes";
+        };
       };
     };
-  };
 
-  services.ssh-agent.enable = true;
+    services.ssh-agent.enable = true;
+  };
 }
