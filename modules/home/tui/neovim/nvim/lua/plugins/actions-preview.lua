a647cb4e2db7726e4e7414a73d7d01207b6e86fa
bytehaufen <rico.ukro@gmx.de>
2025-10-11 06:06:50 +0200

    feat(nvim): add actions-preview.nvim

---
+++ modules/home/tui/neovim/nvim/lua/plugins/actions-preview.lua
@@ -0,0 +1,44 @@
+return {
+  -- Disable LazyVim's existing
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- Disable the default <leader>ca keybind
+      keys[#keys + 1] = { "<leader>ca", false }
+    end,
+  },
+  {
+    "aznhe21/actions-preview.nvim",
+    keys = {
+      {
+        "<leader>ca",
+        function()
+          require("actions-preview").code_actions()
+        end,
+        mode = { "n", "v" },
+        desc = "Code action preview",
+      },
+    },
+    opts = {
+      highlight_command = {
+        function()
+          return require("actions-preview.highlight").delta()
+        end,
+      },
+      telescope = {
+        sorting_strategy = "ascending",
+        layout_strategy = "vertical",
+        layout_config = {
+          width = 0.8,
+          height = 0.9,
+          prompt_position = "top",
+          preview_cutoff = 20,
+          preview_height = function(_, _, max_lines)
+            return max_lines - 15
+          end,
+        },
+      },
+    },
+  },
+}
