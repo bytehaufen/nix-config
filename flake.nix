a240a30262085bb28234f7748f3c19193d6b3b94
Rico Ukro <rico.ukro@gmx.de>
2026-01-06 18:49:58 +0100

    fix: cachix not working

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "My Home Manager configuration";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     agenix = {
       url = "github:yaxitech/ragenix";
