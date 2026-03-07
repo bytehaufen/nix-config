8321bfa62d7e2298fd6add9460ae4f4b6dff172a
Rico Ukro <rico.ukro@gmx.de>
2026-03-07 12:07:45 +0100

    fix(hyprland): add missing guiitls to purge warning

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -4,13 +4,19 @@
   inputs,
   pkgs,
   ...
-}: {
+}: let
+  inherit (pkgs.stdenv.hostPlatform) system;
+in {
   config = lib.mkIf config.opts.nixos.gui.enable {
     programs.hyprland = {
       enable = true;
 
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      package = inputs.hyprland.packages.${system}.hyprland;
+      portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
     };
+
+    environment.systemPackages = [
+      inputs.hyprland.inputs.hyprland-guiutils.packages.${system}.default
+    ];
   };
 }
