efedfe22973aa80ebb12f9c008ea8f95b6ce4594
Rico Ukro <rico.ukro@gmx.de>
2026-06-09 09:40:04 +0200

    feat(nvim): enable built-in plugins

--- modules/home/tui/neovim/nvim/lua/config/lazy.lua
+++ modules/home/tui/neovim/nvim/lua/config/lazy.lua
@@ -90,14 +90,14 @@ require("lazy").setup({
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        "gzip",
+        -- "gzip",
         -- "matchit",
         -- "matchparen",
         -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
+        -- "tarPlugin",
+        -- "tohtml",
         "tutor",
-        "zipPlugin",
+        -- "zipPlugin",
       },
     },
   },
