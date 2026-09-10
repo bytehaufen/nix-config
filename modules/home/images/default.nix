d4f676ea3207fff3ad44755bd6296130cc1fb366
Rico Ukro <rico.ukro@gmx.de>
2026-09-10 13:50:53 +0200

    fix: correct picture links

--- modules/home/images/default.nix
+++ modules/home/images/default.nix
@@ -4,12 +4,10 @@
   ...
 }: {
   config = lib.mkIf config.opts.home.gui.enable {
-    home.file = let
-      link = config.lib.file.mkOutOfStoreSymlink;
-    in {
-      "Pictures/Avatars/mihouse.jpg".source = link ./Avatars/milhouse.jpg;
-      "Pictures/Avatars/bytehaufen.png".source = link ./Avatars/bytehaufen.png;
-      "Pictures/Wallpapers/dark-music.jpg".source = link ./Wallpapers/dark-music.jpg;
+    home.file = {
+      "Pictures/Avatars/mihouse.jpg".source = ./Avatars/milhouse.jpg;
+      "Pictures/Avatars/bytehaufen.png".source = ./Avatars/bytehaufen.png;
+      "Pictures/Wallpapers/dark-music.jpg".source = ./Wallpapers/dark-music.jpg;
     };
   };
 }
