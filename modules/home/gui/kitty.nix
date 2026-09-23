460cfd828dded6ef17a63ac8f1223a2d1eb092eb
Rico Ukro <rico.ukro@gmx.de>
2026-09-23 17:07:39 +0200

    fix(kitty): do not use nerd font

--- modules/home/gui/kitty.nix
+++ modules/home/gui/kitty.nix
@@ -19,8 +19,8 @@
       };
 
       font = {
-        name = "JetBrainsMono NerdFont Mono";
-        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono";
+        package = pkgs.jetbrains-mono;
         size = 10;
       };
 
@@ -39,8 +39,11 @@
 
         background_opacity = 1.0;
         # Kitty detects and uses the semibold version, so we need to override
-        bold_font = "postscript_name=JetBrainsMonoNFM-ExtraBold";
-        bold_italic_font = "postscript_name=JetBrainsMonoNFM-ExtraBoldItalic";
+        bold_font = "postscript_name=JetBrainsMono-ExtraBold";
+        bold_italic_font = "postscript_name=JetBrainsMono-ExtraBoldItalic";
+        symbol_map = ''
+          U+e000-U+e00a,U+e0a0-U+e0a2,U+e0a3,U+e0b0-U+e0b3,U+e0b4-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ea60-U+ec1e,U+ed00-U+efce,U+f000-U+f2ff,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono
+        '';
       };
     };
 
